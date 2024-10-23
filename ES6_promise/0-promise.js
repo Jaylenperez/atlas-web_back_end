32ff9647cfd66ba16d1242dec51a82bf587a1c56
jaylenperez <jaylenestella24@gmail.com>
2024-10-22 19:59:35 -0500

    Test

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+// 0-promise.js
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Response from API");
+        }, 1000);
+    });
+}
+
+export default getResponseFromAPI;
