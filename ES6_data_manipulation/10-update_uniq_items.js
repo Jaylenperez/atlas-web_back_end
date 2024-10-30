31dc8536f1e37fe00b19a2bb3fb5fb3178c3395e
jaylenperez <jaylenestella24@gmail.com>
2024-10-30 13:04:56 -0500

    eslint

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+export default function updateUniqueItems(map) {
+  // Check if the argument is a Map
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  // Iterate through the entries of the Map
+  for (const [key, value] of map.entries()) {
+    // Update the quantity if it is 1
+    if (value === 1) {
+      map.set(key, 100); // Set the new quantity to 100
+    }
+  }
+}
