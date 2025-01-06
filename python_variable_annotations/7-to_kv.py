c0db2b6f13ceae85e810ae59b2b7ec165bdf4e5e
jaylenperez <jaylenestella24@gmail.com>
2025-01-06 15:12:20 -0600

    Created function that takes a string k and a number v the function returns a tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module defines a function to_kv, which returns a tuple.
+The tuple contains a string and the square of a number.
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple where the first element is a string k and the second element is
+    the square of v (which can be int or float), annotated as a float.
+
+    Args:
+        k (str): The string value.
+        v (Union[int, float]): The value to be squared, either int or float.
+
+    Returns:
+        Tuple[str, float]: A tuple with the string and the square of the number (as float).
+    """
+    return (k, float(v ** 2))
