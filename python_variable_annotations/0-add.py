fb25a63e04063fdb10fb478f7f2c26ba1a66e372
jaylenperez <jaylenestella24@gmail.com>
2025-01-06 13:19:06 -0600

    pycode style fixes (added lines and removed whitespace)

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,14 +3,15 @@
 This module provides a type-annotated function to sum two floats.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Returns the sum of two float numbers.
-    
+
     Args:
         a (float): The first float.
         b (float): The second float.
-    
+
     Returns:
         float: The sum of two floats.
     """
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,6 +3,7 @@
 This module provides a type-annotated function to concatenate two strings.
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Concatenates two strings and returns the result.
