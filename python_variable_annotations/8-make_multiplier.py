cf36dff98df5fac9e21fe9da857c43b54b8c25e2
jaylenperez <jaylenestella24@gmail.com>
2025-01-06 15:25:31 -0600

    added blank line and shorten line 16 for pycode

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,15 +5,16 @@ Module defines function make_multiplier that returns a multiplier function.
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Returns a function that multiplies a float by a specified multiplier.
-    
+
     Args:
         multiplier (float): The value to multiply by.
-    
+
     Returns:
-        Callable[[float], float]: Function that multiplies float by the multiplier.
+        Callable[[float], float]: Function multiplies float by multiplier.
     """
     # The returned function multiplies its argument by the multiplier
     def multiplier_function(n: float) -> float:
