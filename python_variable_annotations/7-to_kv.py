dbd74d60aebe8dccdbe8336960932a2adba3ca27
jaylenperez <jaylenestella24@gmail.com>
2025-01-06 15:13:56 -0600

    shorten lines for pycode

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -9,14 +9,17 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a tuple where the first element is a string k and the second element is
-    the square of v (which can be int or float), annotated as a float.
+    Returns a tuple where the first element is a string k
+    the second element is the square of v (which can be int or float),
+    annotated as a float.
 
     Args:
         k (str): The string value.
-        v (Union[int, float]): The value to be squared, either int or float.
+        v (Union[int, float]): The value to be squared,
+        either int or float.
 
     Returns:
-        Tuple[str, float]: A tuple with the string and the square of the number (as float).
+        Tuple[str, float]: A tuple with the string and the square
+        of the number (as float).
     """
     return (k, float(v ** 2))
