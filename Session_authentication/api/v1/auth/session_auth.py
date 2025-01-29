d7a373314307be1ba5e07383595790d0108e031a
jaylenperez <jaylenestella24@gmail.com>
2025-01-29 13:26:54 -0600

    created SessionAuth class and imported it in app.py

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module for Session Authentication class
+"""
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """ Session Authentication class
+    Inherits from Auth. For now, it doesn't add any new functionality.
+    """
+    pass
