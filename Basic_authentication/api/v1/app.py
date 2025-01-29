b8ceb751aec8313ce4c4e02b415d0c41deda19d7
jaylenperez <jaylenestella24@gmail.com>
2025-01-28 19:18:03 -0600

    pycodestyle

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -28,14 +28,15 @@ def before_request():
     """
     if auth is None:
         return None
-    
+
     excluded_paths = ['/api/v1/status/', '/api/v1/status', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     if request.path not in excluded_paths:
         if auth.authorization_header(request) is None:
-            abort(401) # Unauthorized error
+            abort(401)
         if auth.current_user(request) is None:
-            abort(403) # Forbidden error
+            abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
