10edd31fb84110313478d6c48534ab5d6dec8d6c
jaylenperez <jaylenestella24@gmail.com>
2025-01-28 19:51:24 -0600

    pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,14 +7,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic authentication class inheriting from Auth"""
-    
+
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
         Extracts the Base64 part from the Authorization header for Basic Authentication
-        
+
         Arguments:
             authorization_header (str): The Authorization header from the request
-        
+
         Returns:
             str: The Base64 part of the Authorization header or None
         """
