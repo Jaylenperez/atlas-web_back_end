430bf7f8db5f470c941ba8bdc07f662b97855449
jaylenperez <jaylenestella24@gmail.com>
2025-01-29 12:53:55 -0600

    Add /users/me endpoint and update /users/<user_id> for current user retrieval

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -27,10 +27,25 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id =='me':
+        if request.current_user is None:
+            abort(404)
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def view_me() -> str:
+    """ GET /api/v1/users/me
+    Return the current authenticated user.
+    """
+    if request.current_user is None:
+        abort(404)
+    return jsonify(request.current_user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
