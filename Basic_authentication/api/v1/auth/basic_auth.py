2e4ae879218d0891d8be6e949a300ca4e05041a7
jaylenperez <jaylenestella24@gmail.com>
2025-01-29 11:30:50 -0600

    Implemented current_user method in BasicAuth for user authentication

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -98,3 +98,33 @@ class BasicAuth(Auth):
             return None
 
         return user
+    
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the User instance for a request.
+
+        Arguments:
+            request: The request object.
+
+        Returns:
+            User instance if authentication is successful, otherwise None.
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_auth_header = self.extract_base64_authorization_header(auth_header)
+        if base64_auth_header is None:
+            return None
+
+        decoded_auth_header = self.decode_base64_authorization_header(base64_auth_header)
+        if decoded_auth_header is None:
+            return None
+
+        email, password = self.extract_user_credentials(decoded_auth_header)
+        if email is None or password is None:
+            return None
+
+        return self.user_object_from_credentials(email, password)
