e1d1e405c9922e712b3aa402701b0814271b79de
jaylenperez <jaylenestella24@gmail.com>
2025-01-30 11:54:22 -0600

    Created a SQLAlchemy model named User for a database table named users using mapping declaration of SQLAlchemy

---
+++ user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+User module for authentication service
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False, unique=True)
+    hashed_password = Column(String(250), nullable=False, unique=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
+
+    def __repr__(self):
+        return "<User(email='%s', hashed_password='$s', session_id='$s', reset_token='$s')>" % (
+                            self.email, self.hashed_password, self.session_id, self.reset_token)
