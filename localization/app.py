02fc48da1109dd123a354f4a0545f079701a40cf
jaylenperez <jaylenestella24@gmail.com>
2025-02-04 17:18:46 -0600

    documentation

--- localization/app.py
+++ localization/app.py
@@ -7,13 +7,25 @@ app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_TRANSLATION_DIRECTORIES'] = './translations'
            
 def get_locale():
+    """
+    Returns the best match for a supported locale based on the 'Accept-Language'
+    header in the incoming requests.
+    
+    Returns:
+        str: The best match language code ('en' or 'fr').
+    """
     return request.accept_languages.best_match(['en', 'fr'])
 
 babel = Babel(app, locale_selector=get_locale)
 
 @app.route('/')
 def index():
-    """ Returns translated message based on locale."""
+    """
+    Returns translated message based on client's locale.
+
+    Returns:
+        str: Translated greeting message.
+    """
     return _("Hello, World!")
 
 
