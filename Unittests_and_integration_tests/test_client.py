d02a0dbda6f04f191bf9ab2f5c1f22a7fe227097
jaylenperez <jaylenestella24@gmail.com>
2025-02-04 01:07:52 -0600

    parameterize and test has_license method in GithubOrgClient

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -60,5 +60,16 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with("https://api.github.com/orgs/google/repos")
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """Test GithubOrgClient.has_license method with parameterized values"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
+
+
 if __name__ == "__main__":
     unittest.main()
