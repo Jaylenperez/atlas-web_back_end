5f4ca13b11424109939fa8c629178526a1c12d7e
jaylenperez <jaylenestella24@gmail.com>
2025-02-04 17:12:35 -0600

    added request from flask, get_locale now checks for preffered language

--- localization/app.py
+++ localization/app.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from flask_babel import Babel, _
 
 app = Flask(__name__)
-babel = Babel(app)
 
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_TRANSLATION_DIRECTORIES'] = './translations'
            
-@babel.localeselector
-def getlocale():
-    return 'en'
+def get_locale():
+    return request.accept_languages.best_match(['en', 'fr'])
+
+babel = Babel(app, locale_selector=get_locale)
 
 @app.route('/')
 def index():
+    """ Returns translated message based on locale."""
     return _("Hello, World!")
 
 
