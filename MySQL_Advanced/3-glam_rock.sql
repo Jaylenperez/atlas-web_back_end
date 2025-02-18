c89a20fa2f345419b0ae51be8861bbbc6cce911f
jaylenperez <jaylenestella24@gmail.com>
2025-02-18 01:55:04 -0600

    corrected for when formed and split are integers

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,9 @@
--- List Glam rock bands ranked by longevity, handling NULL split dates
+-- List Glam rock bands ranked by longevity (formed/split are YEAR integers)
 
 SELECT band_name, 
        CASE
-           WHEN split IS NULL THEN YEAR(CURDATE()) - YEAR(formed)  -- Use current year if split is NULL
-           ELSE YEAR(split) - YEAR(formed)
+           WHEN split IS NULL THEN YEAR(CURDATE()) - formed  -- Use current year if split is NULL
+           ELSE split - formed
        END AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
