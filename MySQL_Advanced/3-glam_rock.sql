c26bdc69d9212a570a636aaa5e630ccfa8008f8b
jaylenperez <jaylenestella24@gmail.com>
2025-02-18 02:03:44 -0600

    ensure formed and split columns are date or datetime type

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- List all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, 
-       (YEAR(split) - YEAR(formed)) AS lifespan 
-FROM metal_bands 
-WHERE main_style = 'Glam rock' 
+SELECT band_name, IFNULL(split, YEAR(CURDATE())) - formed AS lifespan 
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
