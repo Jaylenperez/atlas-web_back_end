f788d59a3952772ef23e526d5397929eaec42cbb
jaylenperez <jaylenestella24@gmail.com>
2025-02-19 16:25:54 -0600

    task 9

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -14,5 +14,4 @@ def insert_school(mongo_collection, **kwargs):
     Returns:
         The new _id of the inserted document.
     """
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_id
