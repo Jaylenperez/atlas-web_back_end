5714b2fe81f2b27feb25f49b46dd781e219970f4
jaylenperez <jaylenestella24@gmail.com>
2025-02-19 16:27:32 -0600

    try this

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -14,4 +14,6 @@ def insert_school(mongo_collection, **kwargs):
     Returns:
         The new _id of the inserted document.
     """
-    return mongo_collection.insert_one(kwargs).inserted_id
+    result = mongo_collection.insert_one(kwargs)
+
+    return result.inserted_id
