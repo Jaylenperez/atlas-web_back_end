f4153d1e29902569edd1129955b23ae1f9d5609a
jaylenperez <jaylenestella24@gmail.com>
2025-03-06 20:24:26 -0600

    task 0 Node redis client

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+    // Displays a message when connected
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+    console.error('Redis client not connected to the server:', err);
+});
